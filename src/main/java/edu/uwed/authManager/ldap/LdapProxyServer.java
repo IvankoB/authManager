54daaca3288dd31911e03e8c794a14a7d323b8da
IvankoB <IvankoB@users.noreply.github.com>
2025-04-03 12:39:41 +0500

    + progress fixes

--- src/main/java/edu/uwed/authManager/ldap/LdapProxyServer.java
+++ src/main/java/edu/uwed/authManager/ldap/LdapProxyServer.java
@@ -13,6 +13,8 @@ import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ssl.SslBundle;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.stereotype.Component;
 
 import javax.net.ssl.SSLException;
@@ -27,44 +29,46 @@ public class LdapProxyServer {
     private final ConfigProperties configProperties;
     private final LdapRequestHandler ldapRequestHandler;
 
+//    private final SslBundles sslBundles;
+    private final SslContext sslContext;
+
     private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
     private Channel ldapChannel;
     private Channel ldapsChannel;
 
     @PostConstruct
-    public void start() throws CertificateException, InterruptedException {
-        SelfSignedCertificate ssc = new SelfSignedCertificate();
-        SslContext sslContext;
+    public void start() throws InterruptedException {
         try {
-            sslContext = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
-        } catch (SSLException e) {
-            throw new RuntimeException(e);
+            SslBundle sslBundle = sslBundles.getBundle("dc01LdapProxy"); // строка 36
+            SslContext sslContext = sslBundle.createSslContext();        // строка 37
+
+            bossGroup = new NioEventLoopGroup(1);                        // строка 38
+            workerGroup = new NioEventLoopGroup();                       // строка 39
+            int ldapPort = configProperties.getLdapProxy().getPortLdap();
+            int ldapsPort = configProperties.getLdapProxy().getPortLdaps();
+            long maxMessageSize = configProperties.getLdapProxy().getMaxMessageSize();
+
+            ServerBootstrap ldapBootstrap = new ServerBootstrap();
+            ldapBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new LdapServerInitializer(ldapRequestHandler, sslContext, false, maxMessageSize))
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ServerBootstrap ldapsBootstrap = new ServerBootstrap();
+            ldapsBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new LdapServerInitializer(ldapRequestHandler, sslContext, true, maxMessageSize))
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ldapChannel = ldapBootstrap.bind(ldapPort).sync().channel();
+            ldapsChannel = ldapsBootstrap.bind(ldapsPort).sync().channel();
+            logger.info("Started LDAP on port {} and LDAPS on port {}", ldapPort, ldapsPort);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize LDAPS server", e);
         }
-
-        bossGroup = new NioEventLoopGroup(1);
-        workerGroup = new NioEventLoopGroup();
-        int ldapPort = configProperties.getLdapProxy().getPortLdap();
-        int ldapsPort = configProperties.getLdapProxy().getPortLdaps();
-        long maxMessageSize = configProperties.getLdapProxy().getMaxMessageSize();
-
-        ServerBootstrap ldapBootstrap = new ServerBootstrap();
-        ldapBootstrap.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new LdapServerInitializer(ldapRequestHandler, sslContext, false, maxMessageSize))
-                .option(ChannelOption.SO_BACKLOG, 128)
-                .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-        ServerBootstrap ldapsBootstrap = new ServerBootstrap();
-        ldapsBootstrap.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new LdapServerInitializer(ldapRequestHandler, sslContext, true, maxMessageSize))
-                .option(ChannelOption.SO_BACKLOG, 128)
-                .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-        ldapChannel = ldapBootstrap.bind(ldapPort).sync().channel();
-        ldapsChannel = ldapsBootstrap.bind(ldapsPort).sync().channel();
-        logger.info("Started LDAP on port {} and LDAPS on port {}", ldapPort, ldapsPort);
     }
 
     @PreDestroy
